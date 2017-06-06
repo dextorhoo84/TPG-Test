84dcead4c68c037cfbfda9e6102de0700ca0ef54
dextorhoo84 <pekyong.hoo@gmail.com>
2017-06-06 15:11:37 +0800

    Update IPValidationRegex.java

--- IPValidation/IPValidationRegex.java
+++ IPValidation/IPValidationRegex.java
@@ -14,6 +14,11 @@ public class IPValidationRegex {
 		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
 		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
 	
+    /**
+     * Validate IP address with regular expression by using Java Pattern and Matcher
+     * @param ip
+     * @return boolean result of the validation
+     */
 	public boolean validate(String ip) {
 		
 		pattern = Pattern.compile(IPADD_PATTERN);
